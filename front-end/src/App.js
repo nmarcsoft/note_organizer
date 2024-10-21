bd0f0015ddfbebdd558816d82d6b32d31d042538
Nicolas MARC <nicolas.marc@epita.fr>
2024-10-21 16:40:48 +0200

    feat: toggle arborescence

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import './App.css';
-import logo from './noter.png';
+import logo from './noter.png'; // Assurez-vous que l'image est importée
 import ApiService from './service/ApiService.js';
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
   const [error, setError] = useState(null);
   const [selectedContent, setSelectedContent] = useState('');
   const [expandedDirectories, setExpandedDirectories] = useState(new Set());
+  const [isDataListVisible, setIsDataListVisible] = useState(true); // État pour contrôler la visibilité du volet
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
@@ -53,6 +54,10 @@ function App() {
     setSelectedContent(content);
   };
 
+  const toggleDataListVisibility = () => {
+    setIsDataListVisible(!isDataListVisible);
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
@@ -69,35 +74,37 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="top-left-logo">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" onClick={toggleDataListVisibility} style={{ cursor: 'pointer' }} />
         </div>
         <div className="app-container">
-          <div className="data-list">
-            {Object.keys(groupedData).length > 0 ? (
-              <ul>
-                {Object.entries(groupedData).map(([directory, items]) => (
-                  <li key={directory}>
-                    <p onClick={() => toggleDirectory(directory)} style={{ cursor: 'pointer', fontWeight: 'bold' }}>
-                      {directory} {expandedDirectories.has(directory) ? '▼' : '▶'}
-                    </p>
-                    {expandedDirectories.has(directory) && (
-                      <ul>
-                        {items.map((item) => (
-                          <li key={item.name}>
-                            <p onClick={() => handleNoteClick(item.content)} style={{ cursor: 'pointer' }}>
-                              {item.name}
-                            </p>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>Aucune note disponible</p>
-            )}
-          </div>
+          {isDataListVisible && (
+            <div className="data-list" style={{ width: '250px' }}> {/* Ajustez la largeur ici */}
+              {Object.keys(groupedData).length > 0 ? (
+                <ul>
+                  {Object.entries(groupedData).map(([directory, items]) => (
+                    <li key={directory}>
+                      <p onClick={() => toggleDirectory(directory)} style={{ cursor: 'pointer', fontWeight: 'bold' }}>
+                        {directory} {expandedDirectories.has(directory) ? '▼' : '▶'}
+                      </p>
+                      {expandedDirectories.has(directory) && (
+                        <ul>
+                          {items.map((item) => (
+                            <li key={item.name}>
+                              <p onClick={() => handleNoteClick(item.content)} style={{ cursor: 'pointer' }}>
+                                {item.name}
+                              </p>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>Aucune note disponible</p>
+              )}
+            </div>
+          )}
           <div className="main-content">
             <ReactMarkdown remarkPlugins={[remarkGfm]}>{selectedContent}</ReactMarkdown>
           </div>
